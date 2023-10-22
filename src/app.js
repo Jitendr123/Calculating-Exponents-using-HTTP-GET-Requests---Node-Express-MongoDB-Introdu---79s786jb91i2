8539aaa804e7b6c06645c9ec60c164c2f398a5f1
JitendraKrenai <129717689+JitendraKrenai@users.noreply.github.com>
2023-10-22 15:26:27 +0530

    done

--- src/app.js
+++ src/app.js
@@ -18,13 +18,13 @@ const server = http.createServer((req, res) => {
       // If the input is valid, the API should return a 200 OK status code along with the exponential result in the response body.  
       // If the input is invalid, the API should return a 400 Bad Request status code along with an error message.
       if(!value1 || !value2){
-        res.writeHead(400);
+        res.writeHead(400,{"content-type":"text/html"});
         res.end('invalid response')
       }else if(num1<=0){
-        res.writeHead(404);
+        res.writeHead(404,{"content-type":"text/html"});
         res.end('The operation cannot be performed')
       }else{
-        res.writeHead(200);
+        res.writeHead(200,{"content-type":"text/html"});
         res.end("Here is the result",Math.pow(num1,num2));
       }
       
