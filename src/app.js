2fc571a006bd2dcc5bc068691eaa007dc7b5e562
JitendraKrenai <129717689+JitendraKrenai@users.noreply.github.com>
2023-10-22 15:03:29 +0530

    done

--- src/app.js
+++ src/app.js
@@ -13,6 +13,20 @@ const server = http.createServer((req, res) => {
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
+      // "num1" must be a positive integer. If it is zero or negative, the API should return a 404 Not Found status code along with an error message: The operation cannot be performed
+      // "num2" must be a non-negative integer.   
+      // If the input is valid, the API should return a 200 OK status code along with the exponential result in the response body.  
+      // If the input is invalid, the API should return a 400 Bad Request status code along with an error message.
+      if(!value1 || !value2){
+        res.writeHead(400);
+        res.end('invalid response')
+      }else if(num1<=0){
+        res.writeHead(404);
+        res.end('The operation cannot be performed')
+      }else{
+        res.writeHead(200);
+        res.end(Math.pow(num1,num2));
+      }
       
     });
     }
